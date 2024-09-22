0fc6b8e2c782729b6b799227b3b39444bd7796b3
Andy Lo <andylolu24@gmail.com>
2024-09-22 16:16:06 +0200

    Remove unused files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(
+        {
+            remarkPlugins: [remarkMath],
+            rehypePlugins: [[rehypeKatex, {
+                macros: {}
+            }]]
+        }
+    ), sitemap()],
 });
